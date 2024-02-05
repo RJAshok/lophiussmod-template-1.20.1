d4ed7cd9610f4221006d0de63a61da8cf563d72a
Rohan J Ashok <rohanjohnashok@gmail.com>
2024-02-05 14:09:39 +0530

    Initial commit

---
+++ src/main/java/net/lophius/lophiussmod/LophiusModClient.java
@@ -0,0 +1,11 @@
+package net.lophius.lophiussmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LophiusModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
